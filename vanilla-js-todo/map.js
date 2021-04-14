4c4b1423586225d8acda2ae7871059443874ef98
LaNice <lanicemp10@gmail.com>
2021-04-14 13:50:29 -0400

    updaded map practice

--- vanilla-js-todo/map.js
+++ vanilla-js-todo/map.js
@@ -1,9 +1,17 @@
+const each = require("./each")
+
+
+
+
 const map = (collection, cb) => {
   let result = [];
-  for (let i = 0; i < collection.length; i++) {
-      let element = collection[i]; 
-      result.push(cb(element))
-  }
+  each(collection, (value, index)=>{
+    result.push(cb(value, index, collection))
+  })
+  // for (let i = 0; i < collection.length; i++) {
+  //     let element = collection[i]; 
+  //     result.push(cb(element))
+  
   return result 
 };
 
@@ -12,5 +20,35 @@ const numbers = [1, 2, 3];
  return number * 2;
 });
 console.log(numarray)
+
+
+const map2 =(arr, cb)=>{
+    let array = []
+    for(let i = 0 ; i<arr.length; i++){
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
